2e5995678e9fe521a9242abc1a0d2688644d3c61
rahim iqbal <superiqu@gmail.com>
2020-08-06 17:06:42 +0600

    user route open for admin

--- app.js
+++ app.js
@@ -48,8 +48,6 @@ app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
