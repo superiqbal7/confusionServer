97b800c69930a3d9370520f44ae0465818eba4be
rahim iqbal <superiqu@gmail.com>
2020-08-06 14:36:09 +0600

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,6 +3,18 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema ({
+  firstname: {
+    type: String,
+    default: ''
+  },
+  lastname: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
   admin: {
     type: Boolean,
     default: false
